0f9ceb6529843798c0079e398ede61cb211c25fc
Eguo Wang <wangeguo@gmail.com>
2025-03-21 11:32:57 +0800

    refactor(solidity): use walkdir instead of implementing file traversal manually

--- crates/solidity-detector/src/bin/solidity-detector-file.rs
+++ crates/solidity-detector/src/bin/solidity-detector-file.rs
@@ -12,41 +12,27 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::path::Path;
-
 use hummanta_detection::{command, DetectContext, DetectResult, Detector};
+use walkdir::WalkDir;
 
 pub struct SolidityFileDetector;
 
 /// Implements the Detector trait for SolidityFileDetector.
 ///
-/// Detects Solidity files by checking if the path ends with ".sol" or includes
-/// such files in its first level.
+/// Detects Solidity files by verifying if any file in the specified path has a
+/// ".sol" extension.
 impl Detector for SolidityFileDetector {
     fn detect(&self, context: &DetectContext) -> DetectResult {
-        let path = &context.path;
-
-        if is_solidity_file(path) {
+        if WalkDir::new(&context.path)
+            .into_iter()
+            .filter_map(Result::ok)
+            .any(|entry| entry.path().extension().is_some_and(|ext| ext == "sol"))
+        {
             return DetectResult::pass("Solidity".to_string());
-        }
-
-        if path.is_dir() {
-            if let Ok(entries) = path.read_dir() {
-                for entry in entries.flatten() {
-                    if is_solidity_file(&entry.path()) {
-                        return DetectResult::pass("Solidity".to_string());
-                    }
-                }
-            }
         }
 
         DetectResult::fail()
     }
-}
-
-/// Check if the file is a Solidity file.
-fn is_solidity_file(path: &Path) -> bool {
-    path.extension().is_some_and(|ext| ext == "sol")
 }
 
 /// Run the Solidity file detector.
