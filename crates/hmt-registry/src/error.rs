a504e9cab4254aaa796df5258600cd718349fe9b
Eguo Wang <wangeguo@gmail.com>
2025-04-16 14:46:38 +0800

    feat(registry): basic scaffolding and simple registry client impls

---
+++ crates/hmt-registry/src/error.rs
@@ -0,0 +1,48 @@
+// Copyright (c) The Hummanta Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::io;
+use thiserror::Error;
+
+pub type Result<T> = std::result::Result<T, RegistryError>;
+
+#[derive(Error, Debug)]
+pub enum RegistryError {
+    #[error("network error: {0}")]
+    NetworkError(#[from] reqwest::Error),
+
+    #[error("I/O error: {0}")]
+    IoError(#[from] io::Error),
+
+    #[error("toml parse error: {0}")]
+    TomlError(#[from] toml::de::Error),
+
+    #[error("manifest not found: {0}")]
+    ManifestNotFound(String),
+
+    #[error("invalid registry path: {0}")]
+    InvalidPath(String),
+
+    #[error("unsupported registry protocol: {0}")]
+    UnsupportedProtocol(String),
+
+    #[error("package not found: {0}")]
+    PackageNotFound(String),
+
+    #[error("release version not found: {0} v{1}")]
+    ReleaseNotFound(String, String),
+
+    #[error("other error: {0}")]
+    Other(String),
+}
